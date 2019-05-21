77609852a33ed9a3027ffcd2758256aacdf0e9bc
qazwsxedc1038 <43199513+qazwsxedc1038@users.noreply.github.com>
2019-05-22 00:37:04 +0800

    Create code.js

---
+++ code.js
@@ -0,0 +1,19 @@
+javascript:
+form1.GV2$ctl02$RB.value = "B001      ";
+form1.GV2$ctl03$RB.value = "C001      ";
+form1.GV3$ctl02$RB.value = "A005      ";
+form1.GV3$ctl03$RB.value = "A005      ";
+form1.GV3$ctl04$RB.value = "A005      ";
+form1.GV3$ctl05$RB.value = "A005      ";
+form1.GV3$ctl06$RB.value = "A005      ";
+form1.GV3$ctl07$RB.value = "A005      ";
+form1.GV3$ctl08$RB.value = "A005      ";
+form1.GV3$ctl09$RB.value = "A005      ";
+form1.GV3$ctl10$RB.value = "A005      ";
+form1.GV3$ctl11$RB.value = "A005      ";
+form1.GV3$ctl12$RB.value = "A005      ";
+form1.GV3$ctl13$RB.value = "A005      ";
+form1.GV3$ctl14$RB.value = "A005      ";
+form1.GV4$ctl02$RB.value = "A001      ";
+form1.GV4$ctl03$RB.value = "A001      ";
+document.getElementById("BOK").click();
